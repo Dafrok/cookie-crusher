1f8c5bfd43a948162cd96426c3bada094b4d43ba
Dafrok <o.o@mug.dog>
2016-06-13 11:26:11 +0800

    content handler

--- src/content.js
+++ src/content.js
@@ -1,7 +1,42 @@
 var bigCookie = document.getElementById('bigCookie')
-
-function oneClick(cookie) {
-  cookie.click()
+var timers = {
+    autoClick: null,
+    autoUpgrade: null,
+    autoBonus: null
 }
 
-setInterval(oneClick, 1, bigCookie)
+function oneClick(cookie) {
+    cookie.click()
+}
+
+function bonusClick() {
+
+}
+
+function upgrade() {
+
+}
+
+function toggleAutoClick (bool) {
+    if (typeof timers.autoClick !== null) {
+        timers.autoClick = null
+    } else {
+        timers.autoClick = setInterval(oneClick, 1, bigCookie)
+    }
+}
+
+function toggleAutoBonus (bool) {
+    if (typeof timers.autoBonus !== null) {
+        timers.autoBonus = null
+    } else {
+        timers.autoBonus = setInterval(bonusClick, 1000)
+    }
+}
+
+function toggleAutoUpgrade (bool) {
+    if (typeof timers.autoUpgrade !== null) {
+        timers.autoUpgrade = null
+    } else {
+        timers.autoUpgrade = setInterval(upgrade, 5000)
+    }
+}
