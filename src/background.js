a001ac7054e0d2a71ef5b5aa138f0739bb8e5373
Dafrok <o.o@mug.dog>
2016-06-16 15:53:08 +0800

    use localStorage

--- src/background.js
+++ src/background.js
@@ -1,3 +1,24 @@
+var flags = {
+    get autoClick () {
+        return !!localStorage.getItem('autoClick')
+    },
+    set autoClick (val) {
+        localStorage.setItem('autoClick', val)
+    },
+    get autoBonus () {
+        return !!localStorage.getItem('autoBonus')
+    },
+    set autoBonus (val) {
+        localStorage.setItem('autoBonus', val)
+    },
+    get autoUpgrade () {
+        return !!localStorage.getItem('autoUpgrade')
+    },
+    set autoUpgrade (val) {
+        localStorage.setItem('autoUpgrade', val)
+    }
+}
+
 chrome.runtime.onConnect.addListener(function (port) {
     port.onMessage.addListener(function (msg) {
         switch (msg) {
@@ -12,7 +33,10 @@ chrome.runtime.onConnect.addListener(function (port) {
                 break
         }
     })
-    port.onError
 })
 
-console.log(localStorage.getItem('auto-click'))
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//     if (tab.url === 'http://orteil.dashnet.org/cookieclicker/') {
+//         chrome.tabs.executeScript({code: 'toggleAutoClick(true)'})
+//     }
+// })
--- src/popup.js
+++ src/popup.js
@@ -5,39 +5,36 @@ var conn = chrome.runtime.connect({
 function getDOM(id) {
     return document.querySelector(id)
 }
-
-var flags = {
-    autoClick: false, // !!localStorage.getItem('autoClick'),
-    autoBonus: false, // !!localStorage.getItem('autoBonus'),
-    autoUpgrade: false //!!localStorage.getItem('autoUpgrade')
-}
+var backgroundPage = chrome.extension.getBackgroundPage()
+var flags = backgroundPage.flags
 
 var btnAutoClick = getDOM('#auto-click')
 var btnAutoBonus = getDOM('#auto-bonus')
 var btnAutoUpgrade = getDOM('#auto-upgrade')
 
-// function initBtn(btn, name) {
-//     if (flags[name]) {
-//         btn.classList.add('active')
-//     } else {
-//         btn.classList.remove('active')
-//     }
-// }
+function initBtn(btn, name) {
+    if (flags[name]) {
+        btn.classList.add('active')
+    } else {
+        btn.classList.remove('active')
+    }
+}
 
-// initBtn(btnAutoClick, 'autoClick')
-// initBtn(btnAutoBonus, 'autoBonus')
-// initBtn(btnAutoUpgrade, 'autoUpgrade')
+initBtn(btnAutoClick, 'autoClick')
+initBtn(btnAutoBonus, 'autoBonus')
+initBtn(btnAutoUpgrade, 'autoUpgrade')
 
 btnAutoClick.onclick = function () {
     conn.postMessage('auto-click')
     if (flags.autoClick) {
         // localStorage.setItem('autoClick', '')
         btnAutoClick.classList.remove('active')
+        flags.autoClick = ''
     } else {
         // localStorage.setItem('autoClick', 'true')
         btnAutoClick.classList.add('active')
+        flags.autoClick = 'true'
     }
-    flags.autoClick = !flags.autoClick
 }
 
 btnAutoBonus.onclick = function () {
